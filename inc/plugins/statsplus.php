211353642293f204bcfbd3fbbb427d319a65031d
Unknown <volca780@gmail.com>
2017-12-31 14:21:22 +0100

    Function: info

---
+++ inc/plugins/statsplus.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @function Return plugin information
+ * @return array
+ */
+function noteplus_info()
+{
+    return [
+        "name"          => "StatsPlus",
+        "description"   => "Display more stats for user.",
+        "author"        => "volca780",
+        "authorsite"    => "https://github.com/volca780",
+        "version"       => "1.0",
+        "compatibility" => "18*"
+    ];
+}
